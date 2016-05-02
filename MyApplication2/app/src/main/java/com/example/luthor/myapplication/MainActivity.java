467fb6cc402e21649931b69a8ad8c825dd645014
Luthor <lucio0314@163.com>
2016-05-02 20:07:26 +0800

    222

--- MyApplication2/app/src/main/java/com/example/luthor/myapplication/MainActivity.java
+++ MyApplication2/app/src/main/java/com/example/luthor/myapplication/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String s = new String();
+        String s1 = new String();
     }
 }
